ab8018b34885056296241d9b38f306d0a2539ed5
Mohamad Sheikh Alshabab <alshabab.moh@gmail.com>
2023-01-09 09:38:48 +0300

    add try & except to functions

--- index.py
+++ index.py
@@ -22,32 +22,44 @@ levels = [
 @app.route('/')
 @app.route('/digimon',methods =['GET'])
 def get_digimon():
-    return render_template('index.html',data=data,levels=levels)
+    try:
+        return render_template('index.html',data=data,levels=levels)
+    except Exception as err:
+        print(err)
 # <------GET ALL DIGIMONs----->
 
 # GET ALL DIGIMONs by level
 @app.route('/digimon/level/<level>',methods =['GET'])
 def get_digimon_by_level(level):
-    digimons_by_level = get_detail_by_level(level)
-    return render_template('index.html',data=digimons_by_level,levels=levels)
+    try:
+        digimons_by_level = get_detail_by_level(level)
+        return render_template('index.html',data=digimons_by_level,levels=levels)
+    except Exception as err:
+        print(err)
 
 # function to filter by level
 def get_detail_by_level(level):
-    api = f'{url}/level/{level}'
-    response_api = requests.get(api)
-    data_api = response_api.json()
-    return data_api
+    try:
+        api = f'{url}/level/{level}'
+        response_api = requests.get(api)
+        data_api = response_api.json()
+        return data_api
+    except Exception as err:
+        print(err)
 # <------GET ALL DIGIMONs by level----->
 
 # function to filter by name
 @app.route('/digimon/name', methods=['GET'])
 def get_digimon_by_name():
-    search_name = request.args.get('name').lower()
-    if search_name:
-        filtered_data = [item for item in data if search_name in item['name'].lower()]
-    else:
-        filtered_data = data
-    return render_template('index.html',data=filtered_data,levels=levels)
+    try:
+        search_name = request.args.get('name').lower()
+        if search_name:
+            filtered_data = [item for item in data if search_name in item['name'].lower()]
+        else:
+            filtered_data = data
+        return render_template('index.html',data=filtered_data,levels=levels)
+    except Exception as err:
+        print(err)
 # <------GET ALL DIGIMONs by name----->
 
 # @app.route('/digimon/<name>', methods=['POST'])
