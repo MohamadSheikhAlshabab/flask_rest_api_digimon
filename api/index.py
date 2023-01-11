161aaf339ab844f0be6cea46dcc37456d89a6dda
Mohamad Sheikh Alshabab <alshabab.moh@gmail.com>
2023-01-11 21:43:01 +0300

    try to deploy

--- api/index.py
+++ api/index.py
@@ -4,11 +4,16 @@ from flask import Flask,request,render_template
 TEMPLATE_DIR = os.path.abspath('../templates')
 STATIC_DIR = os.path.abspath('../static')
 
-app = Flask(__name__ ,template_folder = TEMPLATE_DIR, static_folder = STATIC_DIR)
+app = Flask(__name__,template_folder='../templates',static_folder='../static')
 
 url = 'https://digimon-api.vercel.app/api/digimon'
 response = requests.get(url)
+response.raise_for_status()
 data = response.json()
+if response.status_code != 204:
+    data
+if response.status_code != 404:
+    print('Oh, No! this 404 error')
 levels = [
     'In Training',
     'Training',
@@ -26,7 +31,8 @@ def get_digimon():
     try:
         return render_template('index.html',data=data,levels=levels)
     except Exception as err:
-        return print(err)
+        print(err)
+        return err
 
 @app.route('/digimon/level/<level>',methods =['GET'])
 def get_digimon_by_level(level):
@@ -34,7 +40,8 @@ def get_digimon_by_level(level):
         digimons_by_level = get_detail_by_level(level)
         return render_template('index.html',data=digimons_by_level,levels=levels)
     except Exception as err:
-        return print(err)
+        print(err)
+        return err
 
 def get_detail_by_level(level):
     try:
@@ -43,7 +50,8 @@ def get_detail_by_level(level):
         data_api = response_api.json()
         return data_api
     except Exception as err:
-        return print(err)
+        print(err)
+        return err
 
 @app.route('/digimon/name', methods=['GET'])
 def get_digimon_by_name():
@@ -55,7 +63,8 @@ def get_digimon_by_name():
             filtered_data = data
         return render_template('index.html',data=filtered_data,levels=levels)
     except Exception as err:
-        return print(err)
+        print(err)
+        return err
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
